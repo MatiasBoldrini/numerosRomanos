5d1229081f9f292d59efc50c8f85cb783ea67ce2
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 <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>
2022-10-17 11:39:29 -0300

    fixed formatting errors

--- test_NN_to_NR.py
+++ test_NN_to_NR.py
@@ -1,102 +1,137 @@
-from numeros_Naturales import *
 import unittest
+
+from numeros_Naturales import *
+
+
 class PrimerTests(unittest.TestCase):
-    def test_1(self): # Importante escribir test primero en el método
+    def test_1(self):  # Importante escribir test primero en el método
         resultado = convertir_a_naturales("I")
-        self.assertEqual(resultado,1)# verificar que el resultado sea 11
-    def test_2(self): 
+        self.assertEqual(resultado, 1)  # verificar que el resultado sea 11
+
+    def test_2(self):
         resultado = convertir_a_naturales("II")
-        self.assertEqual(resultado,2)
-    def test_3(self): 
+        self.assertEqual(resultado, 2)
+
+    def test_3(self):
         resultado = convertir_a_naturales("III")
-        self.assertEqual(resultado,3)
-    def test_4(self): 
+        self.assertEqual(resultado, 3)
+
+    def test_4(self):
         resultado = convertir_a_naturales("IV")
-        self.assertEqual(resultado,4)
-    def test_5(self): 
+        self.assertEqual(resultado, 4)
+
+    def test_5(self):
         resultado = convertir_a_naturales("V")
-        self.assertEqual(resultado,5)
-    def test_6(self): 
+        self.assertEqual(resultado, 5)
+
+    def test_6(self):
         resultado = convertir_a_naturales("VI")
-        self.assertEqual(resultado,6)
-    def test_7(self): 
+        self.assertEqual(resultado, 6)
+
+    def test_7(self):
         resultado = convertir_a_naturales("VII")
-        self.assertEqual(resultado,7)
-    def test_8(self): 
+        self.assertEqual(resultado, 7)
+
+    def test_8(self):
         resultado = convertir_a_naturales("VIII")
-        self.assertEqual(resultado,8)
-    def test_9(self): 
+        self.assertEqual(resultado, 8)
+
+    def test_9(self):
         resultado = convertir_a_naturales("IX")
-        self.assertEqual(resultado,9)
-    def test_10(self): 
+        self.assertEqual(resultado, 9)
+
+    def test_10(self):
         resultado = convertir_a_naturales("X")
-        self.assertEqual(resultado,10)
-    def test_12(self): 
+        self.assertEqual(resultado, 10)
+
+    def test_12(self):
         resultado = convertir_a_naturales("XII")
-        self.assertEqual(resultado,12)
-    def test_13(self): 
+        self.assertEqual(resultado, 12)
+
+    def test_13(self):
         resultado = convertir_a_naturales("XIII")
-        self.assertEqual(resultado,13)
-    def test_15(self): 
+        self.assertEqual(resultado, 13)
+
+    def test_15(self):
         resultado = convertir_a_naturales("XV")
-        self.assertEqual(resultado,15)
-    def test_22(self): 
+        self.assertEqual(resultado, 15)
+
+    def test_22(self):
         resultado = convertir_a_naturales("XXII")
-        self.assertEqual(resultado,22)
-    def test_25(self): 
+        self.assertEqual(resultado, 22)
+
+    def test_25(self):
         resultado = convertir_a_naturales("XXV")
-        self.assertEqual(resultado,25)
-    def test_27(self): 
+        self.assertEqual(resultado, 25)
+
+    def test_27(self):
         resultado = convertir_a_naturales("XXVII")
-        self.assertEqual(resultado,27)
-    def test_38(self): 
+        self.assertEqual(resultado, 27)
+
+    def test_38(self):
         resultado = convertir_a_naturales("XXXVIII")
-        self.assertEqual(resultado,38)
-    def test_39(self): 
+        self.assertEqual(resultado, 38)
+
+    def test_39(self):
         resultado = convertir_a_naturales("XXXIX")
-        self.assertEqual(resultado,39)
-    def test_44(self): 
+        self.assertEqual(resultado, 39)
+
+    def test_44(self):
         resultado = convertir_a_naturales("XLIV")
-        self.assertEqual(resultado,44)
-    def test_52(self): 
+        self.assertEqual(resultado, 44)
+
+    def test_52(self):
         resultado = convertir_a_naturales("LII")
-        self.assertEqual(resultado,52)
-    def test_55(self): 
+        self.assertEqual(resultado, 52)
+
+    def test_55(self):
         resultado = convertir_a_naturales("LV")
-        self.assertEqual(resultado,55)
-    def test_69(self): 
+        self.assertEqual(resultado, 55)
+
+    def test_69(self):
         resultado = convertir_a_naturales("LXIX")
-        self.assertEqual(resultado,69)
-    def test_71(self): 
+        self.assertEqual(resultado, 69)
+
+    def test_71(self):
         resultado = convertir_a_naturales("LXXI")
-        self.assertEqual(resultado,71)
-    def test_88(self): 
+        self.assertEqual(resultado, 71)
+
+    def test_88(self):
         resultado = convertir_a_naturales("LXXXVIII")
-        self.assertEqual(resultado,88)
-    def test_100(self): 
+        self.assertEqual(resultado, 88)
+
+    def test_100(self):
         resultado = convertir_a_naturales("C")
-        self.assertEqual(resultado,100)    
-    def test_155(self): 
+        self.assertEqual(resultado, 100)
+
+    def test_155(self):
         resultado = convertir_a_naturales("CLV")
-        self.assertEqual(resultado,155)
-    def test_399(self): 
+        self.assertEqual(resultado, 155)
+
+    def test_399(self):
         resultado = convertir_a_naturales("CLXXVII")
-        self.assertEqual(resultado,177)
-    def test_399(self): 
+        self.assertEqual(resultado, 177)
+
+    def test_399(self):
         resultado = convertir_a_naturales("CLXXXIX")
-        self.assertEqual(resultado,189)
-    def test_222(self): 
+        self.assertEqual(resultado, 189)
+
+    def test_222(self):
         resultado = convertir_a_naturales("CCXXII")
-        self.assertEqual(resultado,222)
-    def test_350(self): 
+        self.assertEqual(resultado, 222)
+
+    def test_350(self):
         resultado = convertir_a_naturales("CCCL")
-        self.assertEqual(resultado,350)
-    def test_350(self): 
+        self.assertEqual(resultado, 350)
+
+    def test_350(self):
         resultado = convertir_a_naturales("CCCLXVI")
-        self.assertEqual(resultado,366)
-    
-    def test_399(self): 
+        self.assertEqual(resultado, 366)
+
+    def test_399(self):
         resultado = convertir_a_naturales("CCCXCIX")
-        self.assertEqual(resultado,399)
-if __name__ == '__main__':
+        self.assertEqual(resultado, 399)
+
+
+if __name__ == "__main__":
     unittest.main()
--- test_NR_to_NN.py
+++ test_NR_to_NN.py
@@ -1,105 +1,137 @@
 import unittest
-from numeros_Romanos import *
-class PrimerTests(unittest.TestCase):
 
+from numeros_Romanos import *
+
+
+class PrimerTests(unittest.TestCase):
     def test_1(self):
         resultado = convertir_a_romanos(1)
-        self.assertEqual(resultado,"I") 
-    def test_2(self): 
+        self.assertEqual(resultado, "I")
+
+    def test_2(self):
         resultado = convertir_a_romanos(2)
-        self.assertEqual(resultado,"II")
-    def test_3(self): 
+        self.assertEqual(resultado, "II")
+
+    def test_3(self):
         resultado = convertir_a_romanos(3)
-        self.assertEqual(resultado,"III")
-    def test_4(self): 
+        self.assertEqual(resultado, "III")
+
+    def test_4(self):
         resultado = convertir_a_romanos(4)
-        self.assertEqual(resultado,"IV")
-    def test_5(self): 
+        self.assertEqual(resultado, "IV")
+
+    def test_5(self):
         resultado = convertir_a_romanos(5)
-        self.assertEqual(resultado,"V")
-    def test_6(self): 
+        self.assertEqual(resultado, "V")
+
+    def test_6(self):
         resultado = convertir_a_romanos(6)
-        self.assertEqual(resultado,"VI")
-    def test_7(self): 
+        self.assertEqual(resultado, "VI")
+
+    def test_7(self):
         resultado = convertir_a_romanos(7)
-        self.assertEqual(resultado,"VII")
-    def test_8(self): 
+        self.assertEqual(resultado, "VII")
+
+    def test_8(self):
         resultado = convertir_a_romanos(8)
-        self.assertEqual(resultado,"VIII")
-    def test_9(self): 
+        self.assertEqual(resultado, "VIII")
+
+    def test_9(self):
         resultado = convertir_a_romanos(9)
-        self.assertEqual(resultado,"IX")
-    def test_10(self): 
+        self.assertEqual(resultado, "IX")
+
+    def test_10(self):
         resultado = convertir_a_romanos(10)
-        self.assertEqual(resultado,"X")
-    def test_12(self): 
+        self.assertEqual(resultado, "X")
+
+    def test_12(self):
         resultado = convertir_a_romanos(12)
-        self.assertEqual(resultado,"XII")
-    def test_13(self): 
+        self.assertEqual(resultado, "XII")
+
+    def test_13(self):
         resultado = convertir_a_romanos(13)
-        self.assertEqual(resultado,"XIII")
-    def test_15(self): 
+        self.assertEqual(resultado, "XIII")
+
+    def test_15(self):
         resultado = convertir_a_romanos(15)
-        self.assertEqual(resultado,"XV")
-    def test_22(self): 
+        self.assertEqual(resultado, "XV")
+
+    def test_22(self):
         resultado = convertir_a_romanos(22)
-        self.assertEqual(resultado,"XXII")
-    def test_25(self): 
+        self.assertEqual(resultado, "XXII")
+
+    def test_25(self):
         resultado = convertir_a_romanos(25)
-        self.assertEqual(resultado,"XXV")
-    def test_27(self): 
+        self.assertEqual(resultado, "XXV")
+
+    def test_27(self):
         resultado = convertir_a_romanos(27)
-        self.assertEqual(resultado,"XXVII")
-    def test_38(self): 
+        self.assertEqual(resultado, "XXVII")
+
+    def test_38(self):
         resultado = convertir_a_romanos(38)
-        self.assertEqual(resultado,"XXXVIII")
-    def test_39(self): 
+        self.assertEqual(resultado, "XXXVIII")
+
+    def test_39(self):
         resultado = convertir_a_romanos(39)
-        self.assertEqual(resultado,"XXXIX")
-    def test_44(self): 
+        self.assertEqual(resultado, "XXXIX")
+
+    def test_44(self):
         resultado = convertir_a_romanos(44)
-        self.assertEqual(resultado,"XLIV")
-    def test_52(self): 
+        self.assertEqual(resultado, "XLIV")
+
+    def test_52(self):
         resultado = convertir_a_romanos(52)
-        self.assertEqual(resultado,"LII")
-    def test_55(self): 
+        self.assertEqual(resultado, "LII")
+
+    def test_55(self):
         resultado = convertir_a_romanos(55)
-        self.assertEqual(resultado,"LV")
-    def test_69(self): 
+        self.assertEqual(resultado, "LV")
+
+    def test_69(self):
         resultado = convertir_a_romanos(69)
-        self.assertEqual(resultado,"LXIX")
-    def test_71(self): 
+        self.assertEqual(resultado, "LXIX")
+
+    def test_71(self):
         resultado = convertir_a_romanos(71)
-        self.assertEqual(resultado,"LXXI")
-    def test_88(self): 
+        self.assertEqual(resultado, "LXXI")
+
+    def test_88(self):
         resultado = convertir_a_romanos(88)
-        self.assertEqual(resultado,"LXXXVIII")
-    def test_100(self): 
+        self.assertEqual(resultado, "LXXXVIII")
+
+    def test_100(self):
         resultado = convertir_a_romanos(100)
-        self.assertEqual(resultado,"C")
-    
-    def test_155(self): 
+        self.assertEqual(resultado, "C")
+
+    def test_155(self):
         resultado = convertir_a_romanos(155)
-        self.assertEqual(resultado,"CLV")
-    def test_399(self): 
+        self.assertEqual(resultado, "CLV")
+
+    def test_399(self):
         resultado = convertir_a_romanos(177)
-        self.assertEqual(resultado,"CLXXVII")
-    def test_399(self): 
+        self.assertEqual(resultado, "CLXXVII")
+
+    def test_399(self):
         resultado = convertir_a_romanos(189)
-        self.assertEqual(resultado,"CLXXXIX")
-    def test_222(self): 
+        self.assertEqual(resultado, "CLXXXIX")
+
+    def test_222(self):
         resultado = convertir_a_romanos(222)
-        self.assertEqual(resultado,"CCXXII")
-    def test_350(self): 
+        self.assertEqual(resultado, "CCXXII")
+
+    def test_350(self):
         resultado = convertir_a_romanos(350)
-        self.assertEqual(resultado,"CCCL")
-    def test_350(self): 
+        self.assertEqual(resultado, "CCCL")
+
+    def test_350(self):
         resultado = convertir_a_romanos(366)
-        self.assertEqual(resultado,"CCCLXVI")
-    
-    def test_399(self): 
+        self.assertEqual(resultado, "CCCLXVI")
+
+    def test_399(self):
         resultado = convertir_a_romanos(399)
-        self.assertEqual(resultado,"CCCXCIX")
-        
-if __name__ == '__main__':
+        self.assertEqual(resultado, "CCCXCIX")
+
+
+if __name__ == "__main__":
     unittest.main()
